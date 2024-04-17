41f1f0ee40ca236374ef6941d188445f0ed6534d
Mido <75834715+mido-maverick@users.noreply.github.com>
2024-04-17 22:20:19 +0800

    Update 3d-palette.js
    Fix geometry rotation

--- posts/3d-palette/3d-palette.js
+++ posts/3d-palette/3d-palette.js
@@ -1,14 +1,16 @@
 function main() {
     const renderer = new THREE.WebGLRenderer();
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(45, 3/2, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(30, 3/2, 0.1, 1000);
     const gridHelper = createGridHelper();
     const axesHelper = new THREE.AxesHelper(1);
     const skybox = createSkybox();
-    const colorCellCoordinates = createColorCellCoordinates(6);
-    const cubes = createCubes(colorCellCoordinates);
 
-    const initialCameraPosition = { r: 10, theta: Math.PI / 4, fixedZ: 5 };
+    const colorCellPositions = createColorCellPositions(6);
+    const colorCells = createColorCells(colorCellPositions);
+
+    const initialCameraPosition = { r: 18, theta: -Math.PI / 4, fixedZ: 3 };
+
     updateCameraPosition();
     updateCameraUp();
     updateCameraLookAt();
@@ -16,44 +18,47 @@ function main() {
     renderer.setSize(768, 512);
     document.querySelector('main').appendChild(renderer.domElement);
 
-    cubes.forEach(cube => scene.add(cube));
+    colorCells.forEach(cube => scene.add(cube));
     scene.add(gridHelper, axesHelper, skybox);
-
-    function animate(time) {
-        time *= 0.001;
-        cubes.forEach(cube => {
-            cube.rotation.x = time;
-            cube.rotation.y = time;
-        });
-        renderer.render(scene, camera);
-        requestAnimationFrame(animate);
-    }
 
     animate();
 
+    function animate(time) {
+        time *= 0.001;
+
+        colorCells.forEach(cube => {
+            cube.rotation.x = time;
+            cube.rotation.y = time;
+        });
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(animate);
+    }
+
     function createGridHelper() {
-        const gridHelper = new THREE.GridHelper(12, 12);
+        const gridHelper = new THREE.GridHelper(20, 20);
         gridHelper.rotation.x = Math.PI / 2;
         return gridHelper;
     }
 
     function createSkybox() {
         const loader = new THREE.TextureLoader();
-        const texture = loader.load('skybox_1024x.png');
+        const texture = loader.load('./skybox_1024x.png');
         const skyboxMaterial = new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
         const skyboxGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
         return new THREE.Mesh(skyboxGeometry, skyboxMaterial);
     }
 
-    function createColorCellCoordinates(n) {
+    function createColorCellPositions(n) {
         const colorCellCoordinates = [];
         const rotationMatrix = new THREE.Matrix4().set(
             Math.sqrt(6)/3, -Math.sqrt(6)/6, -Math.sqrt(6)/6, 0,
-                         0,  Math.sqrt(6)/6, -Math.sqrt(6)/6, 0,
+                         0,  Math.sqrt(2)/2, -Math.sqrt(2)/2, 0,
             Math.sqrt(3)/3,  Math.sqrt(3)/3,  Math.sqrt(3)/3, 0,
                          0,               0,               0, 1
         );
-        const translationMatrix = new THREE.Matrix4().makeTranslation(0, 0, -4);
+        const translationMatrix = new THREE.Matrix4().makeTranslation(0, 0, -n/2*Math.sqrt(2));
         for (let x = 0; x < n; x++) {
             for (let y = 0; y < n; y++) {
                 for (let z = 0; z < n; z++) {
@@ -65,7 +70,7 @@ function main() {
         return colorCellCoordinates;
     }
 
-    function createCubes(coordinates) {
+    function createColorCells(coordinates) {
         const cubes = [];
         const cubeSize = 0.25;
         coordinates.forEach(coord => {
