36c1354bb3e537bd4ac5e9b9ba25394a862f7073
Mido <75834715+mido-maverick@users.noreply.github.com>
2024-04-19 13:45:22 +0800

    Update color_cell.js

--- posts/3d-palette/color_cell.js
+++ posts/3d-palette/color_cell.js
@@ -5,7 +5,7 @@ class ColorCell {
         const geometry = new THREE.CircleGeometry(size / 2, 6);
         const material = new THREE.MeshBasicMaterial({ color: color });
         this.mesh = new THREE.Mesh(geometry, material);;
-        this.color;
+        this.color = color;
     }
 }
 
