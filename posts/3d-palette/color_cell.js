df7bb00ce277543f8bff9fa1b01d92ab71388399
Mido <75834715+mido-maverick@users.noreply.github.com>
2024-04-23 17:03:51 +0800

    Update color_cell.js

--- posts/3d-palette/color_cell.js
+++ posts/3d-palette/color_cell.js
@@ -3,26 +3,24 @@
 class ColorCell {
     constructor(size, position, paletteGridSpan) {
         const oklab = culori.oklab('black');
-        oklab.l = position.z / (paletteGridSpan * Math.sqrt(3)) + 0.5;
-        oklab.a = position.x / (paletteGridSpan * Math.sqrt(3));
-        oklab.b = position.y / (paletteGridSpan * Math.sqrt(3));
+        oklab.l = position.z / (paletteGridSpan * Math.sqrt(3)) * 2 + 0.5;
+        oklab.a = position.x / (paletteGridSpan * Math.sqrt(3)) * 2;
+        oklab.b = position.y / (paletteGridSpan * Math.sqrt(3)) * 2;
         const rgb = culori.convertOklabToRgb(oklab);
         this.color = new THREE.Color().setRGB(rgb.r, rgb.g, rgb.b);
         const geometry = new THREE.CircleGeometry(size / 2, 6);
         const material = new THREE.MeshBasicMaterial({ color: this.color });
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.position.copy(position);
-        //console.log('l:', oklab.l.toFixed(3), 'a:', oklab.a.toFixed(3), 'b:', oklab.b.toFixed(3));
-        //console.log('r:', rgb.r.toFixed(3), 'g:', rgb.g.toFixed(3), 'b:', rgb.b.toFixed(3));
     }
 
     isWithinSRGBGamut() {
-        return this.color.r >= 0.0 &&
-            this.color.g >= 0.0 &&
-            this.color.b >= 0.0 &&
-            this.color.r <= 1.0 &&
-            this.color.g <= 1.0 &&
-            this.color.b <= 1.0;
+        return this.color.r.toFixed(6) >= 0.0 &&
+            this.color.g.toFixed(6) >= 0.0 &&
+            this.color.b.toFixed(6) >= 0.0 &&
+            this.color.r.toFixed(6) <= 1.0 &&
+            this.color.g.toFixed(6) <= 1.0 &&
+            this.color.b.toFixed(6) <= 1.0;
     }
 }
 
